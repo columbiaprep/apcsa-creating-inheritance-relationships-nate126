37aac6a48db3ade462b4e0a8a1628814aea3d034
Nathan Rosenstrach <nrosenstrach@gmail.com>
2024-12-09 08:59:39 -0500

    Nathan Lab 4.1

---
+++ src/StatModifiers.java
@@ -0,0 +1,9 @@
+public class StatModifiers {
+    private int strMod;
+    private int dexMod;
+    private int conMod;
+    private int wisMod;
+    private int intMod;
+    private int chaMod;
+
+}
---
+++ src/Stats.java
@@ -0,0 +1,9 @@
+public class Stats {
+    private int strength;
+    private int dexterity;
+    private int con;
+    private int inteligence;
+    private int wisdom;
+    private int charisma;
+
+}
